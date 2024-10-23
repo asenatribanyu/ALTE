a37c2907496d935c3f8038642b01a94413418353
zaldyalfarez <zaldyalfarez@gmail.com>
2024-10-23 14:47:39 +0700

    Deskripsi SOP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -60,14 +60,14 @@ class DatabaseSeeder extends Seeder
         // Artikel::create([
         //     'author' => 'Admin',
         //     'judul' => 'Kata Pengantar',
-        //     'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+        //     'deskripsi' => '',
         //     'file' => 'artikel/Kata Pengantar.pdf'
         // ]);
 
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Panduan Keselamatan',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini adalah Panduan Keselamatan Kegiatan Praktikum, yang berfokus pada keselamatan saat melakukan kegiatan di laboratorium, khususnya terkait listrik. Panduan ini mencakup tips untuk menghindari bahaya listrik, seperti penggunaan alat pelindung diri (APD) yang sesuai, pengecekan peralatan sebelum digunakan, dan langkah-langkah jika terjadi kecelakaan listrik. Praktikan diharapkan selalu waspada, mematuhi aturan, dan melaporkan kerusakan yang berpotensi membahayakan kepada teknisi atau pembimbing.',
             'file' => 'artikel/Panduan Keselamatan.pdf',
             'thumbnail' => 'thumbnail/panduan keselamatan kegiatan praktikum.png'
         ]);
@@ -85,7 +85,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Inventaris Peralatan Laboratorium',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini mengatur Prosedur Inventarisasi Peralatan Laboratorium. Isinya mencakup langkah-langkah dalam pencatatan, pengecekan, dan pendataan peralatan labor yang dilakukan secara berkala setiap 6 bulan. Proses ini melibatkan teknisi atau laboran yang mencatat jumlah, kondisi, dan informasi terkait peralatan, serta kepala laboratorium yang memeriksa dan mengoordinasikan data tersebut dengan program studi. Tujuannya agar peralatan tercatat dengan baik dan dapat dimanfaatkan secara optimal sesuai dengan standar mutu yang ditetapkan.',
             'file' => 'artikel/Prosdur inventaris peralatan labor.pdf',
             'thumbnail' => 'thumbnail/prosedur inventaris peralatan labor.png'
         ]);
@@ -103,7 +103,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Izin Melaksanakan Tugas Akhir di Laboratorium',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini menjelaskan Prosedur Izin Melaksanakan Tugas Akhir di Laboratorium. Isinya mengatur langkah-langkah yang harus diikuti mahasiswa untuk mendapatkan izin menggunakan laboratorium dalam penyelesaian tugas akhir. Proses dimulai dengan pengajuan surat permohonan, diikuti dengan persiapan laboratorium oleh teknisi, dan berakhir dengan pelaksanaan tugas akhir di bawah bimbingan dosen. Setelah tugas selesai, mahasiswa melaporkan ke teknisi dan mengembalikan kokarde yang dipinjam.',
             'file' => 'artikel/Prosedur izin melaksanakan tugas akhir di laboratorium.pdf',
             'thumbnail' => 'thumbnail/prosedur melaksanakan tugas akhir di labor.png'
         ]);
@@ -116,7 +116,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Keselamatan Kegiatan Laboratorium',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini membahas Prosedur Keselamatan Kegiatan di Laboratorium, yang mengatur tata cara kerja yang aman di laboratorium. Isinya mencakup langkah-langkah untuk persiapan, pelaksanaan, dan penyelesaian kegiatan di laboratorium, termasuk prosedur jika terjadi keadaan darurat seperti kebakaran atau gempa. Prosedur ini melibatkan teknisi, pengguna laboratorium, dan pembimbing, serta menekankan pentingnya koordinasi dan kepatuhan terhadap aturan keselamatan selama bekerja di laboratorium.',
             'file' => 'artikel/Prosedur keselamatan kegiatan laboratorium.pdf',
             'thumbnail' => 'thumbnail/prosedur keselamatan kegiatan di labor.png'
         ]);
@@ -133,7 +133,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Pelaksanaan Praktikum',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini menjelaskan Prosedur Pelaksanaan Praktikum. Di dalamnya terdapat panduan tentang bagaimana praktikum dilakukan, mulai dari perencanaan jadwal oleh kepala laboratorium, persiapan alat oleh teknisi, hingga pelaksanaan praktikum oleh mahasiswa dengan bimbingan dosen atau instruktur. Mahasiswa juga dibimbing dalam analisis data dan penulisan laporan. Setelah praktikum selesai, dilakukan ujian dan penilaian, yang kemudian hasilnya dicatat dan diarsipkan. Semua ini bertujuan agar praktikum berjalan lancar dan sesuai dengan standar yang ditetapkan.',
             'file' => 'artikel/prosedur pelaksanaan praktikum.pdf',
             'thumbnail' => 'thumbnail/prosedur pelaksanaan praktikum.png'
         ]);
@@ -150,7 +150,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Peminjaman Peralatan Laboratorium',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini berisi panduan Prosedur Peminjaman Peralatan Laboratorium. Prosesnya dimulai dengan mahasiswa mengajukan surat permohonan peminjaman yang diketahui oleh dosen pembimbing kepada kepala laboratorium. Kemudian, teknisi atau laboran menyiapkan peralatan yang diperlukan dan mengarsipkan bon peminjaman. Mahasiswa menggunakan peralatan di bawah pengawasan dosen atau laboran, dan setelah selesai, mereka harus mengembalikan peralatan tersebut. Teknisi akan memeriksa kondisi alat, dan jika ada kerusakan, mahasiswa dapat dikenakan sanksi. Jika peralatan dalam kondisi baik, bon peminjaman dikembalikan kepada mahasiswa.',
             'file' => 'artikel/Prosedur peminjaman peralatan Labor.pdf',
             'thumbnail' => 'thumbnail/prosedur peminjaman peralatan labor.png'
         ]);
@@ -167,7 +167,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Pengadaan Bahan Habis Pakai Laboratorium',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini berisi panduan tentang Prosedur Pengadaan Bahan Habis Pakai Laboratorium. Di dalamnya dijelaskan langkah-langkah pengadaan bahan yang diperlukan untuk praktikum, mulai dari teknisi atau laboran mengusulkan kebutuhan, hingga pengadaan dan pendistribusian bahan oleh bagian perencanaan. Proses ini melibatkan beberapa pihak seperti kepala laboratorium, program studi, dan jurusan, untuk memastikan bahan tersedia tepat waktu dan sesuai kebutuhan.',
             'file' => 'artikel/Prosedur pengadaan bahan habis pakai laboratorium.pdf',
             'thumbnail' => 'thumbnail/prosedur pengadaan bahan habis pakai labor.png'
         ]);
@@ -179,7 +179,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Prosedur Perbaikan Peralatan Laboratorium',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini menjelaskan langkah-langkah untuk memperbaiki alat laboratorium. Di dalamnya ada panduan tentang cara melaporkan kerusakan, mengecek, memperbaiki, dan mengembalikan alat yang rusak. Tujuannya supaya proses perbaikan alat berjalan lancar dan semua pihak yang terlibat, seperti teknisi, dosen, dan kepala lab, bisa bekerja sama dengan baik agar alat-alat lab selalu siap digunakan.',
             'file' => 'artikel/prosedur perbaikan alat labor.pdf',
             'thumbnail' => 'thumbnail/prosedur perbaikan peralatan labor.png'
         ]);
@@ -192,7 +192,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Sturktur Organisasi',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini menjelaskan Struktur Organisasi Laboratorium. Di dalamnya diuraikan peran dan tanggung jawab masing-masing anggota yang terlibat dalam pengelolaan laboratorium, termasuk kepala laboratorium, dosen pengampu, teknisi, dan dosen koordinator bidang kajian (KBK). Kepala laboratorium bertanggung jawab atas seluruh kegiatan dan aset laboratorium, sedangkan dosen pengampu mengelola pelaksanaan praktikum. Teknisi membantu dalam perawatan alat serta persiapan laboratorium, dan dosen KBK berperan dalam bimbingan dan pengembangan kegiatan laboratorium.',
             'file' => 'artikel/sturktur organisasi.pdf',
             'thumbnail' => 'thumbnail/struktur organisasi.png'
         ]);
@@ -209,7 +209,7 @@ class DatabaseSeeder extends Seeder
         Artikel::create([
             'author' => 'Admin',
             'judul' => 'Tata Tertib Pelaksanaan Praktikum',
-            'deskripsi' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac mi elit velit hendrerit nullam tempus dictum interdum. Ullamcorper porta risus rhoncus nisl donec porttitor senectus cubilia. Inceptos habitasse felis scelerisque hendrerit scelerisque scelerisque. Platea faucibus faucibus ligula magna donec, leo at inceptos. Placerat ridiculus consequat feugiat et dui. Varius feugiat mi; consequat imperdiet ad leo in? Porttitor elit risus tempus aliquet, vel sem.',
+            'deskripsi' => 'Dokumen ini berisi Tata Tertib Pelaksanaan Praktikum yang harus diikuti oleh praktikan selama kegiatan praktikum. Beberapa aturan penting termasuk kewajiban mengikuti seluruh modul percobaan, datang tepat waktu, memakai baju praktikum, dan menjaga peralatan laboratorium. Praktikan juga dilarang menggunakan peralatan tanpa izin instruktur dan harus mematuhi prosedur keselamatan. Pelanggaran terhadap tata tertib ini dapat mengakibatkan praktikum dinyatakan gagal.',
             'file' => 'artikel/tata tertib praktikum.pdf',
             'thumbnail' => 'thumbnail/tata tertib pelaksanaan praktikum.png'
         ]);
